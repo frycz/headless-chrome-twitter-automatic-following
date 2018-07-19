e49c3d28d74f1f3f80f8071d84258d44f85c765e
Adam Sawicki <adam.sawicki@symphony-solutions.eu>
2018-07-19 19:52:02 +0200

    Refactor bot

--- bot.js
+++ bot.js
@@ -30,50 +30,34 @@ async function navigateWebsite (actionData) {
  
     // Navigate to a URL
     await mainTab.goTo(loginPageUrl);
- 
     // Fill an element
     await mainTab.fill(usernameInput, username);
- 
     // Type in an element
     await mainTab.fill(passwordInput, password);
- 
     // Click on a button
     await mainTab.click(loginButton);
- 
     // Log some info in your console
     await mainTab.log('Login button clicked');
-
-    // Wait some time! (2s)
+    // Wait some time
     await mainTab.wait(timeForRedirections);
-    
     await mainTab.log('Redirecting...');
 
     for(let i = 0; i < actionData.length; i++) {
-
       console.log(`Executing action with argument: ${actionData[i]}`);
-      
       // Navigate to a URL
       const actionUrl = getActionUrl(actionData[i]);
 	    await mainTab.goTo(actionUrl);
-
       // Click login button
       await mainTab.click(actionButton);
-
 	    // Wait some time [ms]
 	    await mainTab.wait(timeForRedirections);
-      
-	    await mainTab.log('Success. Redirecting...');
-
-      // Check if all users followed
-      if (i == actionData.length - 1) {
-        process.exit();
-      }    	
-
+	    await mainTab.log('Success. Redirecting...');   	
 	  	await mainTab.wait(timeBetweenActions);
-	}
+    }
 
     // Close the browser
     await browser.close();
+    process.exit();
 }
 
 async function run(bot, ...params) {
